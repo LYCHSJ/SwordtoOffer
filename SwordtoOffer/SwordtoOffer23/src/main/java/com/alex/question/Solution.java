ae64bef10c5b8d63e61e66468ee7aa6a2a05ef53
LYCHSJ <576094432@qq.com>
2018-09-13 11:50:20 +0800

    Update to SwordtoOffer24

---
+++ SwordtoOffer/SwordtoOffer23/src/main/java/com/alex/question/Solution.java
@@ -0,0 +1,41 @@
+package com.alex.question;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Solution {
+
+    private static class BinaryTreeNode{
+        int value;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+        BinaryTreeNode(int x){
+            this.value=x;
+        }
+    }
+
+    public ArrayList<Integer> printFromToptoBottom(BinaryTreeNode root){
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+        LinkedList<BinaryTreeNode> queue = new LinkedList<>();
+        BinaryTreeNode node = root;
+        queue.push(node);
+        while(!queue.isEmpty()){
+            node = queue.pop();
+            list.add(node.value);
+            if(node.left != null){
+                queue.addLast(node.left);
+            }
+            if(node.right != null){
+                queue.addLast(node.right);
+            }
+        }
+
+        return list;
+    }
+}
+// BFS
