8eb25b4cb95a9b5ae2e6f64ec97de77e6de0f0fd
LYCHSJ <576094432@qq.com>
2018-09-17 10:12:28 +0800

    Update to SwordtoOffer24

---
+++ SwordtoOffer/SwordtoOffer24/src/main/java/com/alex/question/Solution.java
@@ -0,0 +1,50 @@
+package com.alex.question;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+
+    private static class BinaryTreeNode{
+        int value;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+        BinaryTreeNode(int x){
+            this.value=x;
+        }
+    }
+
+    public boolean VerifySquenceOfBST(int sequence[]){
+        if(sequence == null){
+            return false;
+        }
+        int length = sequence.length;
+        int root = sequence[length-1];
+        int i=0;
+        for(; i<length-1;i++){
+            if(sequence[i]>root){
+                break;
+            }
+        }
+        int j = i;
+        for(;j<length-1;j++){
+            if(sequence[j]<root){
+                return false;
+            }
+        }
+
+        boolean left=true;
+        if(i>0){
+            left=VerifySquenceOfBST(Arrays.copyOfRange(sequence,0,i));
+        }
+
+        boolean right = true;
+        if(j<length-1){
+            right=VerifySquenceOfBST(Arrays.copyOfRange(sequence,i,length-1));
+        }
+
+        return (left&right);
+
+    }
+}
